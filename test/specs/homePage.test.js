e32568e773b3f156cab6220923226c93e92b942d
Lesly-Nupert <lesly.nupert@gmail.com>
2025-11-02 16:01:31 +0100

    refactor: Replace isDisplayed by toBeDisplayed with expect

--- test/specs/homePage.test.js
+++ test/specs/homePage.test.js
@@ -5,13 +5,13 @@ const HomePage = require('../pageobjects/home.page')
 describe('Home Page Tests', () => {
     it('Home Page displayed with logo and title', async () => {
         await HomePage.openHomePage();
-        await expect(HomePage.homeScreen.isDisplayed());
+        await expect(HomePage.homeScreen).toBeDisplayed();
 
         await HomePage.clickLogo();
-        await expect(HomePage.logo.isDisplayed());
+        await expect(HomePage.logo).toBeDisplayed();
 
         await HomePage.clickHomePageTitle();
-        await expect(HomePage.homePageTitle.isDisplayed());
+        await expect(HomePage.homePageTitle).toBeDisplayed();
     });
 });    
 
